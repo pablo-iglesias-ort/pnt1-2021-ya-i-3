9ef57ae74df7e05f6c6f271c56056639229334bf
Ronquiju <ronquiju@gmail.com>
2021-08-30 19:01:12 -0300

    Clases

---
+++ CarritoDeCompras/CarritoDeCompras/Carrito.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CarritoDeCompras
+{
+    public class Carrito
+    {
+        private string activo;
+        private Cliente cliente;
+        private CarritoItems items;
+        private double subtotal;
+    }
+}
---
+++ CarritoDeCompras/CarritoDeCompras/Cliente.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CarritoDeCompras
+{
+    public class Cliente
+    {
+        private string nombre;
+        private string apellido;
+        private string fechaAlta;
+        private string email;
+        private string DNI;
+        private string telefono;
+        private string direccion;
+        private Compra compras;
+        private Carrito carrito;
+    }
+}
+
