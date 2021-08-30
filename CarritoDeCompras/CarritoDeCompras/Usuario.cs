02ab82535fc2a069b97e968a9981a3a7d52a35da
sebiyivo <sebiyivo@gmail.com>
2021-08-30 15:05:50 -0300

    clase Usuario

---
+++ CarritoDeCompras/CarritoDeCompras/Usuario.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CarritoDeCompras
+{
+    public class Usuario
+    {
+        private string nombre;
+        private string email;
+        private string fechaAlta;
+        private readonly string password;
+    }
+}
