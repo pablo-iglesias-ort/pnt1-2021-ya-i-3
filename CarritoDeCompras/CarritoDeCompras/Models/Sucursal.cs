d7c4a2846a4683f996a6651f2cf0a4d26c0cc602
Lumos2020 <lucas.i.gutierrez@gmail.com>
2021-09-02 20:04:18 -0300

    Se corrigen las referencias

--- CarritoDeCompras/CarritoDeCompras/Models/Sucursal.cs
+++ CarritoDeCompras/CarritoDeCompras/Models/Sucursal.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace CarritoDeCompras
+namespace CarritoDeCompras.Models
+
 {
     public class Sucursal
     {
