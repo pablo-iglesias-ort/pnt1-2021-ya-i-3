cf4c514c14da4838e92c70acd5d1aa3e6148c2eb
Lumos2020 <lucas.i.gutierrez@gmail.com>
2021-10-07 21:48:32 -0300

    DataAnnotation 1

--- CarritoDeCompras/CarritoDeCompras/Models/Carrito.cs
+++ CarritoDeCompras/CarritoDeCompras/Models/Carrito.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,10 +8,12 @@ namespace CarritoDeCompras.Models
 {
     public class Carrito
     {
+        [Key]
         public Guid Id { get; set; }
 
         public string Activo { get; set; }
         
+        [Required]
         public Cliente Cliente { get; set; }
         
         public  List<CarritoItem> Items { get; set; }
