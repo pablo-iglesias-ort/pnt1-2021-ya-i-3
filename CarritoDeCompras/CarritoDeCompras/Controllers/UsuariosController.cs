edb2b30520a41ccc52b1b01581c852ebb937cfbc
Ronquiju <ronquiju@gmail.com>
2021-10-28 19:22:53 -0300

    Actualizacion

---
+++ CarritoDeCompras/CarritoDeCompras/Controllers/UsuariosController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CarritoDeCompras.Controllers
+{
+    public class UsuariosController : Controller
+    {
+
+        private readonly MVC_Entity_FrameworkContext _context;
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ CarritoDeCompras/CarritoDeCompras/Data/MVC_Entity_FrameworkContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CarritoDeCompras.Models
+{
+    public class MVC_Entity_FrameworkContext : DbContext
+    {
+        public MVC_Entity_FrameworkContext(DbContextOptions<MVC_Entity_FrameworkContext> opciones) : base(opciones) { 
+        }
+        public DbSet<Carrito> Carritos { get; set; }
+
+        public DbSet<CarritoItem> CarritoItems { get; set; }
+
+        public DbSet<Categoria> Categorias { get; set; }
+        public DbSet<Cliente> Clientes { get; set; }
+        public DbSet<Compra> Compras { get; set; }
+        public DbSet<Empleado > Empleados { get; set; }
+        public DbSet<Producto> Productos { get; set; }
+        public DbSet<Sucursal> Sucursales { get; set; }
+
+        public DbSet<Usuario> Usuarios { get; set; }
+
+
+    }
+}
