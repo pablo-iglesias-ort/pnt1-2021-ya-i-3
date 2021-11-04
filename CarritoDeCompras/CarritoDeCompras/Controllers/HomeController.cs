f27a24f43c533f43592eb554735f29334f874acd
Lumos2020 <lucas.i.gutierrez@gmail.com>
2021-11-04 20:12:49 -0300

    Se sube cambios

--- CarritoDeCompras/CarritoDeCompras/Controllers/HomeController.cs
+++ CarritoDeCompras/CarritoDeCompras/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using CarritoDeCompras.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System;
@@ -18,11 +19,13 @@ namespace CarritoDeCompras.Controllers
             _logger = logger;
         }
 
+        [AllowAnonymous]
         public IActionResult Index()
         {
             return View();
         }
 
+        [Authorize(Roles = nameof(Rol.Empleado))]
         public IActionResult Privacy()
         {
             return View();
